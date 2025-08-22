bfcefd4adba7f992d7c2c4d56419d95e43e74310
patteri6907-hue <patteri6907@gmail.com>
2025-08-22 13:33:03 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 
   import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
+  import Router from './components/Router';
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Router />
+);
   import "./index.css";
 
   createRoot(document.getElementById("root")!).render(<App />);
