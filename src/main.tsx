b40897bb4d46502bb95c96ae0ee8b473c120638a
patteri6907-hue <patteri6907@gmail.com>
2025-08-22 13:51:17 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import './index.css';
 import { Router } from './components/Router';
 
 import { EquipmentPage } from './pages/EquipmentPage';
-import CalculatorPage from './pages/CalculatorPage';
-import DOPEpage from './pages/DOPEpage';
-import LogPage from './pages/logPage'; // filename is logPage.tsx (lower l)
+import { CalculatorPage } from './pages/CalculatorPage';
+import { DOPEpage } from './pages/DOPEpage';
+import { LogPage } from './pages/logPage'; // filename is logPage.tsx (lower l)
 
 const routes = [
   { path: '/equipment', component: <EquipmentPage /> },
