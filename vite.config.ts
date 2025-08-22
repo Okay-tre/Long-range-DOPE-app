a8caf2f7a9de17e564bdf55a323b545f500b6056
patteri6907-hue <patteri6907@gmail.com>
2025-08-22 13:19:15 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Long-range-DOPE-app/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
