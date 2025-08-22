7cd17da0335e69b74387eae77cce69af5faeac1e
patteri6907-hue <patteri6907@gmail.com>
2025-08-22 13:37:49 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './components/Router'; // 👈 default export from Router.tsx
 import './index.css';
+
+import { Router } from './components/Router';
+
+import EquipmentPage from './pages/EquipmentPage';
+import CalculatorPage from './pages/CalculatorPage';
+import DOPEpage from './pages/DOPEpage';
+import LogPage from './pages/logPage'; // filename is logPage.tsx (lower l)
+
+const routes = [
+  { path: '/equipment', component: <EquipmentPage /> },
+  { path: '/calculator', component: <CalculatorPage /> },
+  { path: '/dope', component: <DOPEpage /> },
+  { path: '/log', component: <LogPage /> },
+];
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router />
+    <Router routes={routes} defaultPath="/equipment" />
   </React.StrictMode>
 );
